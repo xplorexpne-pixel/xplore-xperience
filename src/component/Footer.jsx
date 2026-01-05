14a71c85ed625807b28a32d119e4860eb4de35c2
Anuj <anujduttacodr@gmail.com>
2026-01-06 00:42:02 +0530

    nav and footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,20 +1,36 @@
-export default function Footer() {
-    return (
-        <footer className="footer">
-            <div className="footer-content">
-                <h3>Xplore Xperience</h3>
-                <p>Curated travel experiences for explorers.</p>
+<footer className="footer">
+  <div className="footer-container">
 
-                <div className="footer-links">
-                    <a href="#">Instagram</a>
-                    <a href="#">Facebook</a>
-                    <a href="#">WhatsApp</a>
-                </div>
+    {/* LEFT */}
+    <div className="footer-left">
+      <h3>Xplore Xperience</h3>
+      <p>Curated travel experiences for explorers.</p>
+    </div>
 
-                <p className="footer-copy">
-                    © {new Date().getFullYear()} Xplore Xperience. All rights reserved.
-                </p>
-            </div>
-        </footer>
-    );
-}
+    {/* RIGHT */}
+    <div className="footer-right">
+
+      <div className="footer-col">
+        <h4>About Us</h4>
+        <ul>
+          <li>About Us</li>
+          <li>Our Story</li>
+        </ul>
+      </div>
+
+      <div className="footer-col">
+        <h4>Social Media</h4>
+        <div className="footer-socials">
+          <a className="icon-btn"><i className="ri-instagram-line"></i></a>
+          <a className="icon-btn"><i className="ri-twitter-x-line"></i></a>
+          <a className="icon-btn"><i className="ri-facebook-line"></i></a>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <div className="footer-bottom">
+    © {new Date().getFullYear()} Xplore Xperience. All rights reserved.
+  </div>
+</footer>
