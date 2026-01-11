2b2804e436757fe942e3125b229eee95c941ce76
akash dey <dey3552@gmail.com>
2026-01-11 23:37:52 +0530

    fix: update link text for Package in Navbar component

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
                 </NavLink>
 
                 <NavLink to="/package">
-                    Packages
+                    Package
                 </NavLink>
 
                 <NavLink to="/About">
