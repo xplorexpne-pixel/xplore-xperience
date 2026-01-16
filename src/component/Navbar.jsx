2aecd5d8a162836e8a4a71debdb48680332e930f
Anuj <anujduttacodr@gmail.com>
2026-01-16 19:27:50 +0530

    fix: update responsive navbar styles and improve mobile menu functionality

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,54 +2,43 @@ import { NavLink } from "react-router-dom";
 import { useState } from "react";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <nav className="navbar">
 
-    return (
-        <nav className="navbar">
+      <h2 className="logo">Xplore Xperience</h2>
 
-            <h2 className="logo">Xplore Xperience</h2>
+      {/* NAV LINKS + CLOSE BUTTON INSIDE */}
+      <div className={`nav-links ${menuOpen ? "open" : ""}`}>
 
-           
 
-            {/* NAV LINKS */}
-            <div className={`nav-links ${menuOpen ? "open" : ""}`}>
-                
-                {/* CLOSE BUTTON (MOBILE) */}
-              
+        <NavLink to="/" end onClick={() => setMenuOpen(false)}>
+          Home
+        </NavLink>
 
-                <NavLink to="/" end onClick={() => setMenuOpen(false)}>
-                    Home
-                </NavLink>
+        <NavLink to="/packages" onClick={() => setMenuOpen(false)}>
+          Packages
+        </NavLink>
 
-                <NavLink to="/packages" onClick={() => setMenuOpen(false)}>
-                    Packages
-                </NavLink>
+        <NavLink to="/About" onClick={() => setMenuOpen(false)}>
+          About us
+        </NavLink>
+      </div>
 
-                <NavLink to="/About" onClick={() => setMenuOpen(false)}>
-                    About us
-                </NavLink>
-            </div>
+      {/* BOOK NOW (DESKTOP ONLY) */}
+      <div className="Nav-left">
+        <button className="Book-now-btn glow-btn">Book Now</button>
+      </div>
 
-            {/* BOOK NOW (Desktop Only) */}
-            <div className="Nav-left">
-                <button className="Book-now-btn glow-btn">Book Now</button>
-            </div>
-            
-            {/* HAMBURGER MENU (MOBILE) */}
-            
-            <button
-                className="hamburger-menu"
-                onClick={() => setMenuOpen(true)}
-            >
-                <i class="ri-menu-line"></i>
-            </button>
-              <button
-                    className="close-menu"
-                    onClick={() => setMenuOpen(false)}
-                >
-                   <i className="ri-close-line"></i>
-                </button>
-        </nav>
-    );
+      {/* HAMBURGER BUTTON */}
+      <button
+        className="hamburger-menu"
+        onClick={() => setMenuOpen(true)}
+      >
+        <i className="ri-menu-line"></i>
+      </button>
+
+    </nav>
+  );
 }
