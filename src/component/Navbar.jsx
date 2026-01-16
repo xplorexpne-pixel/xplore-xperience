3d7718db60220a23d4097a4579fc0ffe7fcb1a47
Anuj <anujduttacodr@gmail.com>
2026-01-16 12:58:09 +0530

    feat: implement responsive navbar with hamburger menu for mobile devices
    - Updated package.json to allow for flexible versioning of vite.
    - Added styles for a responsive navbar in Main.css, including mobile menu and hamburger button.
    - Modified Navbar component to manage mobile menu state and added close button functionality.

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,29 +1,55 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 export default function Navbar() {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <nav className="navbar">
+
             <h2 className="logo">Xplore Xperience</h2>
 
-            <div className="nav-links">
-                <NavLink to="/" end>
+           
+
+            {/* NAV LINKS */}
+            <div className={`nav-links ${menuOpen ? "open" : ""}`}>
+                
+                {/* CLOSE BUTTON (MOBILE) */}
+              
+
+                <NavLink to="/" end onClick={() => setMenuOpen(false)}>
                     Home
                 </NavLink>
 
-                <NavLink to="/packages">
+                <NavLink to="/packages" onClick={() => setMenuOpen(false)}>
                     Packages
                 </NavLink>
 
-                <NavLink to="/About">
+                <NavLink to="/About" onClick={() => setMenuOpen(false)}>
                     About us
                 </NavLink>
-
-                {/* { <NavLink to="/bike-rentals">
-                    Bike Rentals
-                </NavLink> } */}
-
             </div>
-            <div className="Nav-left"><button className="Book-now-btn  glow-btn">Book Now</button></div>
+
+            {/* BOOK NOW (Desktop Only) */}
+            <div className="Nav-left">
+                <button className="Book-now-btn glow-btn">Book Now</button>
+            </div>
+            
+            {/* HAMBURGER MENU (MOBILE) */}
+            
+            <button
+                className="hamburger-menu"
+                onClick={() => setMenuOpen(true)}
+            >
+                <i class="ri-menu-line"></i>
+            </button>
+              <button
+                    className="close-menu"
+                    onClick={() => setMenuOpen(false)}
+                >
+                   <i className="ri-close-line"></i>
+                </button>
         </nav>
     );
 }
